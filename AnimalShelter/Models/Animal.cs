ab6a3deaff1fa130ec52e64d716c6a5730d9c608
Shanen Cross <shanencross@yahoo.com>
2021-07-05 14:39:00 -0700

    Add model for animals

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AnimalShelter
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Description { get; set; }
+    }
+}
