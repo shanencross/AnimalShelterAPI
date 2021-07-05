5c92aa9762a4f201a716980744fc6dd0e98d33db
Shanen Cross <shanencross@yahoo.com>
2021-07-05 15:35:05 -0700

    Add additional properties to animal model, including the primary key

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -4,8 +4,13 @@ namespace AnimalShelter
 {
     public class Animal
     {
+        public int AnimalId { get; set; }
         public string Name { get; set; }
         public string Species { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public bool SpayedOrNeutered { get; set; }
         public int Age { get; set; }
         public string Gender { get; set; }
         public string Description { get; set; }
